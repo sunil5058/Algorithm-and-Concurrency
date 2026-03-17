c898f41787fbc8ff58e590e5274ad79c45fa2ee5
sunil-shrestha <sunilshrestha2061@gmail.com>
2026-03-17 11:45:51 +0545

    Task7 Completed

---
+++ Tutorials/Tutorial_1/Task7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
+        printf("Greatest = %d\n", a);
+    else
+        printf("Greatest = %d\n", b);
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b && a > c)
+        printf("Greatest = %d", a);
+    else if (b > c)
+        printf("Greatest = %d", b);
+    else
+        printf("Greatest = %d", c);
+
+    return 0;
+}
